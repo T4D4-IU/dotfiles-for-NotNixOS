66bb473cee72eb28f98ce1ac5a60bd412e7cad57
T4D4 <t4d4.icp@gmail.com>
2025-02-07 16:27:13 +0900

    fix typo

--- configuration.nix
+++ configuration.nix
@@ -208,7 +208,7 @@
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   brave
-  warp.terminal
+  warp-terminal
   vscode
   ];
 
