3352042e1c3b52f0b10ef415dbfe26283cc1303a
T4D4 <t4d4.icp@gmail.com>
2025-02-07 16:25:55 +0900

    add warp and vscode

--- configuration.nix
+++ configuration.nix
@@ -208,6 +208,8 @@
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   brave
+  warp.terminal
+  vscode
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
