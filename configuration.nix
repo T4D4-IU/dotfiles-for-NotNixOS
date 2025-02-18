5c62ac5fed09b3b5aabe4388c6ff367b44b268e9
T4D4 .icp <t4d4.icp@gmail.com>
2025-02-18 11:37:58 +0000

    configuration.nixにnixvimを追加

--- configuration.nix
+++ configuration.nix
@@ -207,9 +207,14 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [
+  environment.systemPackages = [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+    {
+      inputs = {
+        nixvim.url = "github:elythh/nixvim";
+      };
+    }
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
