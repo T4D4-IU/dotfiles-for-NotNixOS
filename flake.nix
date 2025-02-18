1c33332f8b4938a5b915bc29293b9b0503ec7709
T4D4 .icp <t4d4.icp@gmail.com>
2025-02-18 11:12:01 +0000

    flake.nixでnixvimの設定を簡素化し、プログラムの有効化を修正

--- flake.nix
+++ flake.nix
@@ -39,24 +39,7 @@
           ./home.nix
           inputs.nixvim.homeManagerModules.nixvim
           {
-            programs.nixvim = {
-              enable = true;
-              opts = {
-                relativenumber = true;
-                number = true;
-                undofile = true;
-                swapfile = false;
-                incsearch = true;
-                cursorline = true;
-
-                tabstop = 4;
-                shiftwidth = 4;
-                expandtab = true;
-                autoindent = true;
-
-                textwidth = 0;
-                };
-            };
+            programs.nixvim.enable = true;
           }
         ];
       };
