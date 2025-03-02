feac63e6e26acf13d5d5c4816761e195ce27fda3
T4D4 .icp <t4d4.icp@gmail.com>
2025-03-02 12:57:47 +0000

    hyprland.nixにプラグインの設定を追加

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -1,7 +1,17 @@
-{
-  programs.kitty.enable = true; # required for the default Hyprland config
-  wayland.windowManager.hyprland.enable = true; # enable Hyprland
+{pkgs, ...}: {
+    programs.kitty.enable = true; # required for the default Hyprland config
+    wayland.windowManager.hyprland = {
+        enable = true;
+        # set the Hyprland and XDPH packages to null to use the ones from the NixOS module
+        # package = null;
+        # portalPackage = null;
 
-  # Optional, hint Electron apps to use Wayland:
-  # home.sessionVariables.NIXOS_OZONE_WL = "1";
+        plugins = with pkgs.hyprlandPlugins; [
+            # Add plugins here
+            borders-plus-plus
+        ];
+    };
+
+    # Optional, hint Electron apps to use Wayland:
+    # home.sessionVariables.NIXOS_OZONE_WL = "1";
 }
